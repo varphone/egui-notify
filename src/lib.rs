bca5e42a2f939c58926ce89326517d9c3b896a2c
Varphone Wong <varphone@qq.com>
2024-04-09 09:44:19 +0800

    Add Toasts::set_frame supports

--- src/lib.rs
+++ src/lib.rs
@@ -202,6 +202,12 @@ impl Toasts {
         self
     }
 
+    /// Changes the frame to display toasts in.
+    pub fn set_frame(&mut self, frame: Frame) -> &mut Self {
+        self.frame = Some(frame);
+        self
+    }
+
     /// Should toasts be added in reverse order?
     pub const fn reverse(mut self, reverse: bool) -> Self {
         self.reverse = reverse;
